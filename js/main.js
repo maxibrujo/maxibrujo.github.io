9b779151a969b2252c1291b2bdeec73d844f4f01
maxibrujo <63284768+maxibrujo@users.noreply.github.com>
2020-06-04 21:42:27 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
-// navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav--opened');
+navMain.classList.add('main-nav--closed'); 
 
-// navToggle.addEventListener('click',function(){
-//     if (navMain.classList.contains('main-nav--closed')){
-//         navMain.classList.remove('main-nav--closed');
-//         navMain.classList.add('main-nav--opened');
-//     }
-//     else {
-//         navMain.classList.remove('main-nav--opened');
-//         navMain.classList.add('main-nav--closed'); 
-//     }
-// });
+navToggle.addEventListener('click',function(){
+    if (navMain.classList.contains('main-nav--closed')){
+        navMain.classList.remove('main-nav--closed');
+        navMain.classList.add('main-nav--opened');
+    }
+    else {
+        navMain.classList.remove('main-nav--opened');
+        navMain.classList.add('main-nav--closed'); 
+    }
+});
